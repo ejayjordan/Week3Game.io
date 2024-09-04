9f6c40062fc5d858f900b6fe8f6ef5aff505f0c3
jay <emma.jordan@bsu.edu>
2024-09-04 11:49:03 -0400

    trying my best

---
+++ index.js
@@ -0,0 +1,24 @@
+/*import { useState } from 'react';
+
+function Card({value, onCardClick}) {
+    return (
+        <button className="card" onClick={onCardClick}>
+            {value}
+        </button>
+    )
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+        if(calculateWinner(cards) || cards[i]) {
+            return;
+        }
+        const nextCards = cards.slice();
+        if (xIsNext) {
+            nextCards[i] =
+        }
+    }
+}
+export default function Card() {
+    return <button className="card">★</button>
+}*/
